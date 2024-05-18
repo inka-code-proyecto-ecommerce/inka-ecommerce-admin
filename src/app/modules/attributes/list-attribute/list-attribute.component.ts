5a056d2b1c9e3a08ae0316c8bfb400057f15daa8
GabrielMCC2020 <gabrielmcc2002cultural@gmail.com>
2024-05-18 00:03:37 -0500

    listado de atributos

--- src/app/modules/attributes/list-attribute/list-attribute.component.ts
+++ src/app/modules/attributes/list-attribute/list-attribute.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { DeleteAttributeComponent } from '../delete-attribute/delete-attribute.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import Swal from 'sweetalert2';
+import { AttributesService } from '../../attributes/service/attributes.service';
 
 @Component({
   selector: 'app-list-attribute',
@@ -6,5 +10,91 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-attribute.component.scss']
 })
 export class ListAttributeComponent {
+  [x: string]: any;
+  attributes: any = [];
+  search: string = '';
+  totalPages: number = 0;
+  currentPage: number = 1;
 
+  constructor(
+    public attributesService: AttributesService,
+    public modalService: NgbModal,
+  ) {
+    
+  }
+
+  ngOnInit(): void {
+    //this.listAttribute();
+    this.isLoading = this.attributesService.isLoading$;
+  }
+
+  listAttributes(page = 1) {
+    this.attributesService.listAttributes(page, this.search).subscribe((res: any) => {
+      console.log(res);
+      this.attributes = res.attributes.data;
+      this.totalPages = res.total;
+      this.currentPage = page;      
+    })
+  }
+  getNameAttribute(type_attribute:number) {
+    let name_attribute = "";
+    switch (type_attribute) {
+      case 1:
+        name_attribute = "Texto"
+        break;
+      case 2:
+        name_attribute = "Numero"
+        break;
+      case 3:
+        name_attribute = "Seleccionable"
+        break;
+      case 4:
+        name_attribute = "Seleccionable Multiple"
+        break;
+      default:
+        break;
+    }
+
+    return name_attribute;
+  }
+
+  loadPage($event: any) {
+    console.log($event);
+    this.listAttributes($event);
+  }
+
+  openModalCreateAttribute() {
+
+  }
+
+  openModalEditAttribute(attribute:any){
+
+  }
+  
+  deleteAttribute(attribute: any) {
+    Swal.fire({ // Utiliza SweetAlert para confirmar la eliminación
+      title: '¿Estás seguro?',
+      text: '¡No podrás revertir esto!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, eliminarlo!',
+      cancelButtonText: 'Cancelar',
+      customClass: {
+        confirmButton: "btn btn-primary",
+        cancelButton: 'btn btn-danger'
+      }
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.attributesService.deleteAttributes(attribute.id).subscribe((res: any) => {
+          let index = this.attributes.findIndex((item: any) => item.id == attribute.id);
+          this.attributes.splice(index, 1);
+          this.toastr.success('Atributo eliminado satisfactoriamente', 'Eliminado');
+        });
+      }
+    });
+  }
+
+  searchTo() {
+    this.listAttribute();
+  }
 }
--- src/app/modules/attributes/service/attributes.service.ts
+++ src/app/modules/attributes/service/attributes.service.ts
@@ -1,9 +1,63 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject, finalize } from 'rxjs';
+import { URL_SERVICE } from 'src/config/config';
+import { AuthService } from '../../auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AttributesService {
+  deleteAttribute(id: any) {
+    throw new Error('Method not implemented.');
+  }
 
-  constructor() { }
+  isLoading$: Observable<boolean>;
+  isLoadingSubject: BehaviorSubject<boolean>;
+
+  constructor(
+    private http: HttpClient,
+    public authservice: AuthService,
+  ) {
+    this.isLoadingSubject = new BehaviorSubject<boolean>(false);
+    this.isLoading$ = this.isLoadingSubject.asObservable();
+  }
+
+
+  listAttributes(page: number = 1, search: string) {
+    this.isLoadingSubject.next(true);
+    let headers = new HttpHeaders({ 'Authorization': 'Bearer' + this.authservice.token });
+    const URL = URL_SERVICE + "/admin/attributes?page=" + page + "&search=" + search;
+    return this.http.get(URL, { headers: headers }).pipe(
+      finalize(() => this.isLoadingSubject.next(false))
+    )
+  }
+
+  deleteAttributes(attribute_id: string) {
+    this.isLoadingSubject.next(true);
+    let headers = new HttpHeaders({ 'Authorization': 'Bearer' + this.authservice.token });
+    const URL = URL_SERVICE + "/admin/attributes/" + attribute_id;
+    return this.http.delete(URL, { headers: headers }).pipe(
+      finalize(() => this.isLoadingSubject.next(false))
+    )
+  }
+
+  createAttributes(data: any) {
+    this.isLoadingSubject.next(true);
+    let headers = new HttpHeaders({ 'Authorization': 'Bearer' + this.authservice.token });
+    const URL = URL_SERVICE + "/admin/attributes";
+    return this.http.post(URL, data, { headers: headers }).pipe(
+      finalize(() => this.isLoadingSubject.next(false))
+    )
+  }
+
+  updateAttributes(attribute_id: string, data: any) {
+    this.isLoadingSubject.next(true);
+    let headers = new HttpHeaders({ 'Authorization': 'Bearer' + this.authservice.token });
+    const URL = URL_SERVICE + "/admin/attributes/edit/" + attribute_id;
+    return this.http.post(URL, data, { headers: headers }).pipe(
+      finalize(() => this.isLoadingSubject.next(false))
+    )
+  }
 }
+
