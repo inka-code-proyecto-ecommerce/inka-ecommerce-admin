4815e19a26143b6bd0cb6b6bfd968de566763be1
dmamanipar <dmamanipar@unsa.edu.pe>
2024-05-24 12:36:46 -0500

    Avance

--- src/app/modules/sliders/edit-sliders/edit-sliders.component.ts
+++ src/app/modules/sliders/edit-sliders/edit-sliders.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { SlidersService } from '../service/sliders.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-sliders',
@@ -6,5 +9,80 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-sliders.component.scss']
 })
 export class EditSlidersComponent {
+  title: string = '';
+  label: string = '';
+  subtitle: string = '';
+  link: string = '';
+  color: string = '';
+  state: number = 1;
+  image_prev: any = '';
+  file_image: any = null;
+  isLoading: any;
 
+  constructor(
+    public sliderService: SlidersService,
+    public toastr: ToastrService,
+    public actibedRouter: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.isLoading = this.sliderService.isLoading$;
+  }
+
+  processImage($event: any) {
+    const file = $event.target.files[0];
+    if (!file.type.startsWith('image/')) {
+      this.toastr.error('Solamente se aceptan imágenes', 'Error de validación');
+      return;
+    }
+    this.file_image = $event.target.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(this.file_image);
+    reader.onloadend = () => (this.image_prev = reader.result);
+    this.sliderService.isLoadingSubject.next(true);
+    setTimeout(() => {
+      this.sliderService.isLoadingSubject.next(false);
+    }, 50);
+  }
+
+  isLoadingView() {
+    this.sliderService.isLoadingSubject.next(true);
+    setTimeout(() => {
+      this.sliderService.isLoadingSubject.next(false);
+    }, 50);
+  }
+
+  save() {
+    if (!this.title || !this.subtitle || !this.file_image) {
+      this.toastr.error('Ingrese todos los datos correctamente', 'Validación');
+      return;
+    }
+
+    let formData = new FormData();
+    formData.append('title', this.title);
+    if (this.label) {
+      formData.append('icon', this.label);
+    }
+    formData.append('subtitle', this.subtitle + '');
+    formData.append('image', this.file_image);
+    if (this.link) {
+      formData.append('link', this.link);
+    }
+    if (this.color) {
+      formData.append('color', this.color);
+    }
+
+    this.sliderService.createSliders(formData).subscribe((res: any) => {
+      console.log(res);
+
+      this.title = '';
+      this.label = '';
+      this.subtitle = '';
+      this.link = '';
+      this.color = '';
+      this.file_image = null;
+      this.image_prev = '';
+      this.toastr.success("Exito", "El Slider se registró correctamente");
+    });
+  }
 }
--- src/app/modules/sliders/list-sliders/list-sliders.component.ts
+++ src/app/modules/sliders/list-sliders/list-sliders.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import Swal from 'sweetalert2';
+import { SlidersService } from '../service/sliders.service';
 
 @Component({
   selector: 'app-list-sliders',
@@ -6,5 +10,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-sliders.component.scss']
 })
 export class ListSlidersComponent {
+  sliders: any = [];
+  search: string = '';
+  totalPages: number = 0;
+  currentPage: number = 1;
+  isLoading: any = null;
 
+  constructor(
+    public sliderService: SlidersService,
+    public modalService: NgbModal,
+    public toastr: ToastrService,
+  ) {}
+
+  ngOnInit(): void {
+    this.listSliders();
+    this.isLoading = this.sliderService.isLoading$;
+  }
+
+  listSliders(page = 1) {
+    this.sliderService.listSliders(page, this.search).subscribe((resp: any) => {
+      console.log(resp);
+      this.sliders = resp.sliders;
+      this.totalPages = resp.total;
+      this.currentPage = page;
+    })
+  }
+
+  loadPage($event: any) {
+    console.log($event);
+    this.listSliders($event);
+  }
+
+  deleteSlider(category: any) {
+    Swal.fire({ // Utiliza SweetAlert para confirmar la eliminación
+      title: '¿Estás seguro?',
+      text: '¡No podrás revertir esto!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, eliminarlo!',
+      cancelButtonText: 'Cancelar',
+      customClass: {
+        confirmButton: "btn btn-primary",
+        cancelButton: 'btn btn-danger'
+      }
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.sliderService.deleteSlider(category.id).subscribe((res: any) => {
+          let index = this.sliders.findIndex((item: any) => item.id == category.id);
+          this.sliders.splice(index, 1);
+          this.toastr.success('Categoria eliminado satisfactoriamente', 'Eliminado');
+        });
+      }
+    });
+  }
+
+  searchTo() {
+    this.listSliders();
+  }
 }
