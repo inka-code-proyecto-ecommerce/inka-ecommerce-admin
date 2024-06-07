cdc2c99fe93a42742551ffb3c5048e4859bfe417
Juan Lenin Mamani Chura <jleninmchura>
2024-06-07 10:24:44 -0500

    Discount

--- src/config/config.ts
+++
@@ -1,5 +0,0 @@
-import { environment } from "src/environments/environment"
-
-export const URL_BACKEND = environment.URL_BACKEND;
-export const URL_SERVICE = environment.URL_SERVICE;
-export const URL_FRONTEND = environment.URL_FRONTEND;
